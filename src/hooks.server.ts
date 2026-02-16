d0b6c870cb2a42f5ef4504f14612f83d9f003c05
alois-reinstadler <64593856+alois-reinstadler@users.noreply.github.com>
2026-02-16 15:29:44 +0100

    initial commit

---
+++ src/hooks.server.ts
@@ -0,0 +1,33 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { building } from '$app/environment';
+
+// auth
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { auth } from '$lib/server/auth';
+
+// i18n
+import { paraglideMiddleware } from '$lib/paraglide/server';
+
+import type { Handle } from '@sveltejs/kit';
+
+const handleParaglide: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request, locale }) => {
+		event.request = request;
+
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		});
+	});
+
+const handleBetterAuth: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({ headers: event.request.headers });
+
+	if (session) {
+		event.locals.session = session.session;
+		event.locals.user = session.user;
+	}
+
+	return svelteKitHandler({ event, resolve, auth, building });
+};
+
+export const handle: Handle = sequence(handleParaglide, handleBetterAuth);
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+
+	import './layout.css';
+	import '@fontsource-variable/geist';
+	import '@fontsource-variable/geist-mono';
+
+	import favicon from '$lib/assets/favicon.svg';
+
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher defaultMode="dark" />
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+{@render children()}
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>
+			{locale}
+		</a>
+	{/each}
+</div>
